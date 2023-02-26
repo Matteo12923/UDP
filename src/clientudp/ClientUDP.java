41baa8a36ad373cd28692b65d8148d4c536e42d9
Matteo <gambacortacasagrandematteo@gmail.com>
2023-02-26 11:38:34 +0100

    modifche a clientUDP e serverUDP

--- src/clientudp/ClientUDP.java
+++ src/clientudp/ClientUDP.java
@@ -4,17 +4,78 @@
  */
 package clientudp;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
 /**
  *
  * @author hp
  */
 public class ClientUDP {
 
-    /**
-     * @param args the command line arguments
-     */
+   DatagramSocket Socket;
+    InetAddress IP;
+    
+    public ClientUDP() {
+         try {
+             Socket = new DatagramSocket();
+             IP = InetAddress.getByName("localhost");
+         }
+         catch (UnknownHostException e) {
+            System.err.println("Errore DNS!");
+        } catch (SocketException e) {
+            System.err.println("Errore nel socket!");
+        }
+        
+}
+
+public void sendAndReceive() {
+    try {
+
+        byte[] inData = new byte[1024];
+        
+        Dipendente dipendente= new Dipendente("Nome","Cognome",id);
+        
+        //Serializzazione: dall'oggetto allo stream di byte
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        ObjectOutputStream os = new ObjectOutputStream(outputStream);
+        os.writeObject(dipendente);
+        
+        
+        
+        byte[] data = outputStream.toByteArray();
+        
+        //Invio dello stream di byte
+        DatagramPacket sendPacket = new DatagramPacket(data, data.length, IP, 5969);
+        Socket.send(sendPacket);
+        System.out.println("Messaggio per il server");
+        
+        //Ricezione della risposta
+        DatagramPacket inPacket = new DatagramPacket(inData, inData.length);
+        Socket.receive(inPacket);
+        String risposta = new String(inPacket.getData());
+        System.out.println("Risposta dal server: " + risposta);
+        Thread.sleep(2000);
+
+        } catch (UnknownHostException e) {
+            System.err.println("Errore del DNS");
+        } catch (SocketException e) {
+            System.err.println("Errore del socket");
+        } catch (IOException e) {
+            System.err.println("Errore I/O");
+        } catch (InterruptedException e) {
+            System.err.println("Errore del thread");
+    }
+}
     public static void main(String[] args) {
-        // TODO code application logic here
+        ClientUDP client = new ClientUDP();
+        client.sendAndReceive();
     }
     
 }
--- src/clientudp/ServerUDP.java
+++ src/clientudp/ServerUDP.java
@@ -4,9 +4,12 @@
  */
 package clientudp;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 
@@ -17,41 +20,65 @@ import java.net.UnknownHostException;
 public class ServerUDP {
     
     
-    DatagramSocket socket = null;
-    
-    public UDPServer() {
+  DatagramSocket socket = null;
+
+    public ServerUDP() {
         try {
         socket = new DatagramSocket(1789);
         } catch (SocketException e) {
             System.err.println("Errore nel socket!");
     }
-    
-    public void InviaRicevi{
-    try{
-
-byte[] InData =new byte[2048];
-while(true){
-
-DatagramPacket inPacket = new DatagramPacket(InData, InData.length);
-            socket.receive(inPacket);
-            
-            byte[] data =new inPacket.getData();
-
 }
 
+public void sendAndReceive() {
+    try {
+        byte[] incomingData = new byte[1024];
 
-} catch (SocketException e) {
-        System.err.println("Errore nel socket!");
-    } catch (IOException i) {
-        System.err.println("Errore di I/O!");
-    } catch (InterruptedException e) {
-        System.err.println("Errore nel thread sleeping!");
+        while (true) {
+            
+            //Ricezione dello stream di byte
+            DatagramPacket incomingPacket = new DatagramPacket(incomingData, incomingData.length);
+            socket.receive(incomingPacket);
+            
+            byte[] data = incomingPacket.getData();
+            
+            //Deserializzazione: dallo stream di byte all'oggetto
+            ByteArrayInputStream in = new ByteArrayInputStream(data);
+            ObjectInputStream is = new ObjectInputStream(in);
+            
+            try {
+                Dipendente dipendente = (Dipendente) is.readObject();
+                System.out.println("Dal client Dipendente;" + dipendente);
+            } catch (ClassNotFoundException e) {
+                System.err.println("La classe dipendenre non esiste");
+            }
+        
+            //Recupero dei dati del mittente
+            InetAddress IPAddress = incomingPacket.getAddress();
+            int port = incomingPacket.getPort();
+            
+            //Invio della risposta
+            String risposta = "il messaggio è stato ricevuto";
+            byte[] replyByte = risposta.getBytes();
+            DatagramPacket replyPacket = new DatagramPacket(replyByte, replyByte.length, IPAddress, port);
+            socket.send(replyPacket);
+            
+            Thread.sleep(1000);
+            
+           
     }
-    
-    
+
+    } catch (SocketException e) {
+        System.err.println("Errore del socket");
+    } catch (IOException i) {
+        System.err.println("Errore I/O");
+    } catch (InterruptedException e) {
+        System.err.println("Errore nel thread");
+    }
 }
     public static void main(String[] args) {
-        // TODO code application logic here
+          ServerUDP server = new ServerUDP();
+        server.sendAndReceive();
     }
 }
 
